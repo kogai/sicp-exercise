b0299e76c9f238aa3deee6ad21d7d0131f7bd987
shinichi kogai <kogai0121@gmail.com>
2018-02-23 14:08:07 +0900

    add range

---
+++ 2.41.scm
@@ -0,0 +1,13 @@
+; 練習問題 2.41:
+; ある整数n以下の異なる正の整数が大小順に並んだ三つ組i, j, k の中で、合計がある整数sとなるものすべてを見つける手続きを書け。
+
+(define (range n m)
+  (if (= n m)
+    ()
+    (cons n (range (+ n 1) m))))
+
+(define (answer n s)
+  n)
+
+; (display (answer 10 15))
+(display (range 0 10))
